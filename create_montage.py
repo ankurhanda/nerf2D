ab5bcd523853baf852f358663d2f97dfa56a2fd6
Ankur Handa <ahanda@nvidia.com>
2020-03-26 07:42:18 -0700

    update
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- create_montage.py
+++ create_montage.py
@@ -1,13 +1,13 @@
 import glob
 import os
 
-raw_xy  = sorted(glob.glob("./dataset/cool_cows/*raw_xy*.jpg"))
-sin_cos = sorted(glob.glob("./dataset/cool_cows/*sin_cos*.jpg"))
+raw_xy  = sorted(glob.glob("./dataset/house/*raw_xy*.jpg"))
+sin_cos = sorted(glob.glob("./dataset/house/*sin_cos*.jpg"))
 
 count = 0
 for x, y in zip(raw_xy, sin_cos):
     print(count, x, y)
     outfileName = 'evolution_{:04d}.jpg'.format(int(count))
-    os.system("montage ./dataset/cool_cows.jpg {} {} -geometry +0+0 {}".format(y, x, outfileName))
+    os.system("montage ./dataset/house.jpg {} {} -geometry +0+0 {}".format(y, x, outfileName))
 
     count += 1 
