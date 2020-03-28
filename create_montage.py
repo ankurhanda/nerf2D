1421acec6d9b24ce4696a27565114af97a4c6c70
Ankur Handa <ahanda@nvidia.com>
2020-03-28 07:34:13 -0700

    adding glasses image
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- create_montage.py
+++ create_montage.py
@@ -10,7 +10,7 @@ count = 0
 for x, y in zip(raw_xy, sin_cos):
     print(count, x, y)
     outfileName = 'evolution_{:04d}.jpg'.format(int(count))
-    # os.system("montage ./dataset/{}.jpg {} {} -geometry +0+0 {}".format(dataset, y, x, outfileName))
-    os.system("montage {} {} -geometry +0+0 {}".format(y, x, outfileName))
+    os.system("montage ./dataset/{}.jpg {} {} -geometry +0+0 {}".format(dataset, y, x, outfileName))
+    # os.system("montage {} {} -geometry +0+0 {}".format(y, x, outfileName))
 
     count += 1 
